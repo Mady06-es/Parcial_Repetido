37cbe7323cc83c946588af67b8cdca2b9f923dd5
Mady06-es <mr20119@ues.edu.sv>
2026-09-16 11:54:50 -0600

    html

---
+++ index.php
@@ -0,0 +1,28 @@
+<?php
+
+
+
+
+?>
+
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <h1 >Bienvenidos a la cafeteria</h1>
+
+<form action="">
+    <label for="">Bebidas</label>
+    <label for="">Alimentos</label>
+    <label for="">Postres</label>
+</form>
+
+
+
+
+</body>
+</html>
